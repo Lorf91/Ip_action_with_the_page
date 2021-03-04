3252dfed190bea004a0af742cfc607828aeff007
Ivan <lorf1991@gmail.com>
2021-03-04 20:52:11 +0200

    done_lesson_#30

--- js/script.js
+++ js/script.js
@@ -24,3 +24,49 @@ const movieDB = {
     ]
 };
 
+const adv = document.querySelectorAll('.promo__adv img'),
+      promoContent = document.querySelector('.promo__content'),
+      promoGenre = promoContent.querySelector('.promo__genre'),
+      bgImg = promoContent.querySelector('.promo__bg'),
+      promoList = promoContent.querySelector('.promo__interactive-list');
+
+
+//Task#1 function for delete elem on page
+function deleteElem(elem) {
+    elem.forEach(item => {
+        item.remove();
+    })
+}
+
+deleteElem(adv);
+
+//task#2 function for change text elem on page
+function changeText(elem, text) {
+    elem.textContent = `${text}`;
+}
+
+changeText(promoGenre, 'драма');
+
+//task#3 change bg_img on page
+bgImg.style.cssText = `background:url("img/bg.jpg") center center/cover no-repeat;`;
+
+//task4
+movieDB.movies.sort();
+promoList.innerHTML = '';
+movieDB.movies.forEach((item, i) => {
+    promoList.innerHTML += `
+        <li class="promo__interactive-item">${i + 1} ${item}
+            <div class="delete"></div>
+        </li>
+    `;
+});
+
+
+
+
+
+
+
+
+
+//console.log(movieDB.movies);
